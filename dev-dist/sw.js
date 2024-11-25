8ab514db818c51522a370f99ac9720c3f0bff839
jowy99 <jowycarreras@gmail.com>
2024-11-25 12:58:38 +0100

    Time of today

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4cmc1e1aheo"
+    "revision": "0.qpls8k3gql"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,8 @@
-import { useState, useEffect } from 'react';
-import reactLogo from '../assets/react.svg';
-import viteLogo from '/vite.svg';
-import { fetchWeatherData } from '../services/rapidapi.js';
+import Home from '../pages/home.jsx'
 
 function App() {
-  const [weatherData, setWeatherData] = useState(null);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetchWeatherData();
-      setWeatherData(data); // Guarda los datos en el estado
-    };
-
-    fetchData();
-  }, []); // Ejecuta una vez al montar el componente
-
   return (
-    <div>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Weather App</h1>
-      <div className="card">
-        {weatherData ? (
-          <div>
-            <h2>Location: {weatherData.location.name}</h2>
-            <p>Temperature: {weatherData.current.temp_c}°C</p>
-            <p>Condition: {weatherData.current.condition.text}</p>
-          </div>
-        ) : (
-          <p>Loading weather data...</p>
-        )}
-      </div>
-    </div>
+    <Home />
   );
 }
 
