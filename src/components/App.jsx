abb9e33e10c8c745d015b2b7b45f7a43659378ee
jowy99 <jowycarreras@gmail.com>
2024-11-29 12:59:55 +0100

    Day info

--- src/components/App.jsx
+++
@@ -1,9 +0,0 @@
-import Home from '../pages/home.jsx'
-
-function App() {
-  return (
-    <Home />
-  );
-}
-
-export default App;
--- src/layout/body.jsx
+++ src/layout/body.jsx
@@ -1,19 +1,19 @@
 import Now from '../components/rightnow.jsx';
 import Today from '../components/today.jsx';
 import Footer from './footer.jsx';
-import Astro from '../components/astro.jsx';
+import Astro from '../components/dia.jsx';
 
-function body() {
+function Body() {
     return (
-        <main className="font-AlbertSans antialiased dark:bg-zinc-800">
-            <section className='w-full h-screen min-h-screen space-y-8 lg:flex lg:flex-col'>
-                <Now/>
-                <Astro/>
-                <Today/>
+        <main className="font-AlbertSans antialiased dark:bg-zinc-800 min-h-screen flex flex-col">
+            <section className="w-full flex-grow space-y-8 lg:flex lg:flex-col">
+                <Now />
+                <Astro />
+                <Today />
             </section>
-            <Footer/>
+            <Footer />
         </main>
-    )
-};
+    );
+}
 
-export default body;
+export default Body;
