49e38d67f515180b7fb848942b92375bd13a8b4e
jowy99 <jowycarreras@gmail.com>
2025-01-31 12:55:13 +0100

    UI changes

--- src/components/navbar/search.jsx
+++ src/components/navbar/search.jsx
@@ -42,7 +42,7 @@ const Search = ({ onCitySelect }) => {
             <li
               key={index}
               onClick={() => handleCitySelect(suggestion.name)}
-              className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 cursor-pointer"
+              className="px-4 py-2 hover:bg-gray-200 dark:hover:bg-gray-700 dark:text-white cursor-pointer"
             >
               {suggestion.name}, {suggestion.region}
             </li>
